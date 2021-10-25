8314d0f65b03f69ee9193e64cffdf54689d6d8d8
ianlogeais <71567996+ianlogeais@users.noreply.github.com>
2021-10-25 11:44:01 +0200

    Add files via upload

---
+++ Connect4.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Puissance4
+{
+    public class Connect4
+    {
+        private int ligne = 6;
+        public int NombreLigne
+        {
+            get { return ligne; }
+        }
+
+        private int col = 7;
+        public int NombreColonne => col;
+
+        public char[,] tableau = new char[6, 7] {
+            {' ',' ',' ',' ',' ',' ',' ', },
+            {' ',' ',' ',' ',' ',' ',' ', },
+            {' ',' ',' ',' ',' ',' ',' ', },
+            {' ',' ',' ',' ',' ',' ',' ', },
+            {' ',' ',' ',' ',' ',' ',' ', },
+            {' ',' ',' ',' ',' ',' ',' ' }
+        };
+
+        public char Pion(int col, int line) => tableau[line, col];
+
+        public char joueur = 'x';
+        private int joueur1 = 1;
+        public int Joueur1 => joueur1;
+
+        public void Jouer(int col)
+        {
+            if (col > 0 && col < 8)
+            {
+                int dligne = DernierSlot(col - 1);
+                if (dligne != -1)
+                {
+                    tableau[dligne, col - 1] = joueur;
+                    if (Victoire() == '#')
+                    {
+                        if (joueur == 'x')
+                        {
+                            joueur = 'o';
+                            joueur1 = 2;
+                        }
+                        else
+                        {
+                            joueur = 'x';
+                            joueur1 = 1;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine("Numéro de colonne invalide");
+            }
+        }
+
+        // Etape 4 
+        // Améliorer le Jouer pour qu'il détecte la victoire/nul et implémenter gagnant et Ended
+        private int gagnant = 0;
+        public int Gagnant => gagnant;
+
+        public int SlotsDispo()
+        {
+            int slots = 0;
+            for (int y = 0; y < ligne; y++)
+            {
+                for (int x = 0; x < col; x++)
+                {
+                    if (tableau[y, x] == ' ')
+                    {
+                        slots += 1;
+                    }
+                }
+            }
+            return slots;
+        }
+
+        public char Victoire()
+        {
+            for (int i = ligne - 1; i >= 0; --i)
+            {
+
+                for (int ix = col - 1; ix >= 0; --ix)
+                {
+                    try
+                    {
+                        if (tableau[i, ix] == joueur &&
+                        tableau[i - 1, ix - 1] == joueur &&
+                        tableau[i - 2, ix - 2] == joueur &&
+                        tableau[i - 3, ix - 3] == joueur)
+                        {
+                            ended = true;
+                            gagnant = joueur1;
+                            return joueur;
+                        }
+                    }
+                    catch { }
+
+
+                    try
+                    {
+                        if (tableau[i, ix] == joueur &&
+                        tableau[i, ix - 1] == joueur &&
+                        tableau[i, ix - 2] == joueur &&
+                        tableau[i, ix - 3] == joueur)
+                        {
+                            ended = true;
+                            gagnant = joueur1;
+                            return joueur;
+                        }
+                    }
+                    catch { }
+
+                    try
+                    {
+                        if (tableau[i, ix] == joueur &&
+                        tableau[i - 1, ix] == joueur &&
+                        tableau[i - 2, ix] == joueur &&
+                        tableau[i - 3, ix] == joueur)
+                        {
+                            ended = true;
+                            gagnant = joueur1;
+                            return joueur;
+                        }
+                    }
+                    catch { }
+
+
+                    try
+                    {
+                        if (tableau[i, ix] == joueur &&
+                        tableau[i - 1, ix + 1] == joueur &&
+                        tableau[i - 2, ix + 2] == joueur &&
+                        tableau[i - 3, ix + 3] == joueur)
+                        {
+                            ended = true;
+                            gagnant = joueur1;
+                            return joueur;
+                        }
+                    }
+                    catch { }
+
+
+                    try
+                    {
+                        if (tableau[i, ix] == joueur &&
+                        tableau[i, ix + 1] == joueur &&
+                        tableau[i, ix + 2] == joueur &&
+                        tableau[i, ix + 3] == joueur)
+                        {
+                            ended = true;
+                            gagnant = joueur1;
+                            return joueur;
+                        }
+                    }
+                    catch { }
+
+                }
+
+            }
+            if (SlotsDispo() == 0)
+            {
+                ended = true;
+                gagnant = 0;
+                return '"';
+            }
+            return '#';
+        }
+
+        public int DernierSlot(int col)
+        {
+            for (int y = 5; y >= 0; y--)
+            {
+
+                if (tableau[y, col] == ' ')
+                {
+                    return y;
+                }
+
+            }
+            return -1;
+        }
+
+        private bool ended = false;
+        public bool Ended => ended;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Puissance4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Connect4();
+
+            do
+            {
+                Display(game);
+                for (; ; )
+                {
+                    Console.WriteLine($"Player {game.joueur} : Which column 1-{game.NombreColonne} ?");
+
+                    var turn = Console.ReadLine();
+                    int column;
+
+                    if (int.TryParse(turn, out column))
+                    {
+                        game.Jouer(column);
+                        break;
+                    }
+                    Console.Error.WriteLine("Invalid column number.");
+                }
+            }
+            while (!game.Ended);
+            Display(game);
+            if(game.Gagnant == 0)
+            {
+                Console.WriteLine("Draw");
+            }
+            else
+            {
+                Console.WriteLine($"Le joueur {game.Gagnant} l'emporte");
+            }
+        }
+        private static void Display(Connect4 game)
+        {
+            for (int y = 0; y < game.NombreLigne; y++)
+            {
+                for (int x = 0; x < game.NombreColonne; x++)
+                {
+                    Console.Write($"| {game.Pion(x, y)} ");
+                }
+                Console.WriteLine("|");
+                for (int x = 0; x < game.NombreColonne; x++)
+                {
+                    Console.Write($"+---");
+                }
+                Console.WriteLine("+");
+            }
+            for (int x = 0; x < game.NombreColonne; x++)
+            {
+                Console.Write($"  {x + 1} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
